61c63ec761f59745adff4c8aaa9e10e4add17752
dlinah <eng.lina.a92@gmail.com>
2017-04-06 21:21:11 +0200

    l

--- root.js
+++ root.js
@@ -45,7 +45,7 @@ expressServer.use("/authenticate",authRouter);
 ////token middleware
 
  expressServer.use(function (request,response,next) {
-        //  var token =request.headers['x-access-token'];
+          var token =request.headers['x-access-token'];
       //   var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU4ZTYyNWZiM2Q1ZjZiODExZmY4OGI4MSIsImlhdCI6MTQ5MTUwNTQwNH0.uNaS9NDWmrIgPHVNtQHqmfgjXlc7YnvWiUc21a4MbPU"
         if(token!== undefined) {
             jwt.verify(token, APP_SECRET, function (err, decoded) {
