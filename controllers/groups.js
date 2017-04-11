7ecbfce7a94a3326d4974e6f6566829a75a407b0
AmiraMohiey <ameramohiey92@gmail.com>
2017-04-11 17:05:59 +0200

    notification

--- controllers/groups.js
+++ controllers/groups.js
@@ -1,143 +1,143 @@
 var express = require('express');
 var router = express.Router();
-var bodyParser=require("body-parser");
-var postRequestMiddleware=bodyParser.json({limit: '20mb'});
+var bodyParser = require("body-parser");
+var postRequestMiddleware = bodyParser.json({ limit: '20mb' });
 var mongoose = require("mongoose");
 var notifications = require("./notifications");
 
 
-router.use(function(request,response,next){
+router.use(function(request, response, next) {
     // Set Origin to allow other domains to send request
-    response.setHeader("Access-Control-Allow-Origin","*");
+    response.setHeader("Access-Control-Allow-Origin", "*");
     // allow four HTTP method
-    response.setHeader("Access-Control-Allow-Methods","GET,POST,PUT,DELETE");
+    response.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
 
     next();
 });
 //list groups
-router.get("/",function(request,response){
-    var id =request.token._id;
-    var result ={};
+router.get("/", function(request, response) {
+    var id = request.token._id;
+    var result = {};
 
 
-        mongoose.model("groups").find({members:id}).populate('members owner',['username','avatar']).exec(function (err, memberGroups) {
-            if(!err){
-                result.member=memberGroups;
-                mongoose.model("groups").find({owner:id}).populate('members owner',['username','avatar']).exec(function (err, ownerGroups) {
-                    if(!err){
-                        result.owner=ownerGroups;
-                        response.status(200);
-                        response.json(result);
+    mongoose.model("groups").find({ members: id }).populate('members owner', ['username', 'avatar']).exec(function(err, memberGroups) {
+        if (!err) {
+            result.member = memberGroups;
+            mongoose.model("groups").find({ owner: id }).populate('members owner', ['username', 'avatar']).exec(function(err, ownerGroups) {
+                if (!err) {
+                    result.owner = ownerGroups;
+                    response.status(200);
+                    response.json(result);
 
-                    }else{
-                        response.status(404);
-                        response.send("Error");
-                    }
-                })
-            }else{
-                response.status(404);
-                response.send(err);
-            }
-        })
+                } else {
+                    response.status(404);
+                    response.send("Error");
+                }
+            })
+        } else {
+            response.status(404);
+            response.send(err);
+        }
+    })
 })
 
-router.get('/:id',function(request,response){
-  var id =request.params.id;
-  mongoose.model("groups").find({_id:id}).populate("members owner",['uaername','avatar']).exec(function (err,group) {
-      if(!err){
-          response.status(200);
-          response.json(group[0]);
-      }else{
-          response.status(404);
-          response.send(err);
-      }
-  })
+router.get('/:id', function(request, response) {
+    var id = request.params.id;
+    mongoose.model("groups").find({ _id: id }).populate("members owner", ['uaername', 'avatar']).exec(function(err, group) {
+        if (!err) {
+            response.status(200);
+            response.json(group[0]);
+        } else {
+            response.status(404);
+            response.send(err);
+        }
+    })
 })
 
 
 //add groups
-router.post("/",postRequestMiddleware,function(request,response){
-    var groupModel=mongoose.model("groups");
-    groupModel.findOne({name:request.body.name, owner: request.token._id},function(err,found){
+router.post("/", postRequestMiddleware, function(request, response) {
+    var groupModel = mongoose.model("groups");
+    groupModel.findOne({ name: request.body.name, owner: request.token._id }, function(err, found) {
         if (found) {
             response.status(200);
-            response.json({success: false, error: 'Group name already exists'});
+            response.json({ success: false, error: 'Group name already exists' });
         } else {
-            var group = new groupModel({name:request.body.name, owner: request.token._id});
-            group.save(function (err) {
-                if(!err){
+            var group = new groupModel({ name: request.body.name, owner: request.token._id });
+            group.save(function(err) {
+                if (!err) {
+                    console.log("groups", group)
+                    notifications.sendnotif([3], { group: group, user: request.token._id })
                     response.status(200);
-                    response.json({success: true});
-                }else{
+                    response.json({ success: true });
+                } else {
                     response.status(404);
-                    response.json({success: false, error: 'request failed please try again later'});
+                    response.json({ success: false, error: 'request failed please try again later' });
                 }
-                
+
             });
 
-            notifications.sendnotif([1,3],{group:group,user:request.token._id})
             console.log(group._id)
         }
     })
 });
 //cancel group
-router.delete("/:id",function(request,response){
-    mongoose.model("groups").remove({_id:request.params.id,  owner: request.token._id},function(err,group){
-        if(!err){
+router.delete("/:id", function(request, response) {
+    mongoose.model("groups").remove({ _id: request.params.id, owner: request.token._id }, function(err, group) {
+        if (!err) {
             response.status(200);
-            response.json({success: true});
-        }else{
+            response.json({ success: true });
+        } else {
             response.status(404);
-            response.json({success: false, error: 'request failed please try again later'});
+            response.json({ success: false, error: 'request failed please try again later' });
         }
     });
 });
 
 //update group
-router.post('/addMember',postRequestMiddleware,function (request,response) {
-    var id =request.body.id;
-    mongoose.model("users").find({email:request.body.email},function(err,user){
-        if(user.length == 0){
-            response.json({success:false,error:"No such user"});
-        }
-        else{
-            mongoose.model("groups").update({_id:id},{$push:{members: user[0]._id}},function(err,groups){
-                if(!err){
-                    notifications.sendnotif([2],{group:groups[0],userId:user[0]._id})
+router.post('/addMember', postRequestMiddleware, function(request, response) {
+    var id = request.body.id;
+    mongoose.model("users").find({ email: request.body.email }, function(err, user) {
+        if (user.length == 0) {
+            response.json({ success: false, error: "No such user" });
+        } else {
+            mongoose.model("groups").findOneAndUpdate({ _id: id }, { $push: { members: user[0]._id } }, function(err, groups) {
+                if (!err) {
+                    notifications.sendnotif([1, 2], { group: groups, userId: user[0]._id })
                     response.status(200);
-                    response.json({success: true});
-                }else{
+                    response.json({ success: true });
+                } else {
                     response.status(404);
-                    response.json({success: false,error:"No such user"});
+                    response.json({ success: false, error: "No such user" });
                 }
             })
         }
     })
 })
-router.post('/deleteMember',postRequestMiddleware,function (request,response) {
-    var id =request.body.id;
+router.post('/deleteMember', postRequestMiddleware, function(request, response) {
+    var id = request.body.id;
     console.log(id, request.body.userid);
-    mongoose.model("groups").update({_id:request.body.id},{$pull:{members: request.body.userid}},function(err,groups){
+    mongoose.model("groups").update({ _id: request.body.id }, { $pull: { members: request.body.userid } }, function(err, groups) {
 
-        if(!err){
+        if (!err) {
             response.status(200);
-            response.json({success: true});
-        }else{
+            response.json({ success: true });
+        } else {
             response.status(404);
-            response.json({success: false,error:"try again later"});
+            response.json({ success: false, error: "try again later" });
         }
     })
-   
+
 });
-router.post('/',postRequestMiddleware,function (request, response) {
-    var group=request.body;
-    var groupModel=mongoose.model("group");
-    var groupM=new groupModel(group);
-    groupM.save(function (err) {
-        if(!err){
+router.post('/', postRequestMiddleware, function(request, response) {
+    var group = request.body;
+    var groupModel = mongoose.model("group");
+    var groupM = new groupModel(group);
+    groupM.save(function(err) {
+        if (!err) {
             console.log("success");
-        }else{
-            console.log("Error",err);
+        } else {
+            console.log("Error", err);
         }
 
     });
--- controllers/notifications.js
+++ controllers/notifications.js
@@ -30,161 +30,171 @@ var notifMsg = function(notification) {
     return { msg: msg, notif_id: notification._id }
 }
 
-const msgs=['[u]: is now friend with :[u]',
-     '[u]: added you to :[g]',
-     '[u]: was added to :[g]: group',
-     '[u]: created :[g]',
-     '[u]: invitated you to :[o]: order',
-     '[u]: made an order :[o]',
-     'your :[o]: was sent',
-     'your :[o]: was deleted',
-     '[u]: added you as friend',
-     'your group :[g]: was invited to :[o]: order'
+const msgs = ['[u]: is now friend with :[u]',
+    '[u]: added you to :[g]',
+    '[u]: was added to :[g]: group',
+    '[u]: created :[g]',
+    '[u]: invitated you to :[o]: order',
+    '[u]: made an order :[o]',
+    'your :[o]: was sent',
+    'your :[o]: was deleted',
+    '[u]: added you as friend',
+    'your group :[g]: was invited to :[o]: order'
 ];
 //
-var addnotif =function (types,parts) {
+var addnotif = function(types, parts) {
 
     console.log(parts.user);
-        console.log('in if')
-        mongoose.model("users").find({_id:parts.user},function (err,user) {
-            console.log(parts);
-            if(!err){
-                var notification={seen:[]};
-                if(user.length!==0)
-                var friends=user[0].friends;
-                //console.log('friends',friends);
-                types.forEach(function (type) {
+    console.log('in if')
+    mongoose.model("users").find({ _id: parts.user }, function(err, user) {
+        console.log("parts", parts);
+        if (!err) {
+            var notification = { seen: [] };
+            if (user.length !== 0)
+                var friends = user[0].friends;
+            //console.log('friends',friends);
+            types.forEach(function(type) {
 
-                    switch (type){
-                        case 0 :{
+                switch (type) {
+                    case 0:
+                        {
                             //deprecated
                             break;
                         }
-                        case 1 :{
-                            if(parts.group.members.length!==0){
-                                notification.to=parts.group.members;
-                                for (var i=0;i<parts.group.members.length;i++){
-                                    notification.seen[i]={seen:false,id:parts.group.members[i]};
+                    case 1:
+                        {
+                            if (parts.group.members.length !== 0) {
+                                notification.to = parts.group.members;
+                                for (var i = 0; i < parts.group.members.length; i++) {
+                                    notification.seen[i] = { seen: false, id: parts.group.members[i].toString() };
                                 }
-                                notification.userId=parts.user;
-                                notification.groupId=parts.group._id;
-                                notification.message=msgs[1];
+                                notification.userId = parts.user;
+                                notification.groupId = parts.group._id;
+                                notification.message = msgs[1];
                             }
                             break;
                         }
-                        case 2 :{
-                            notification.userId=parts.userId;
-                            notification.to=parts.group.members;
-                            for (var i=0;i<notification.to.length;i++){
-                                notification.seen[i]={seen:false,id:notification.to[i]};
+                    case 2:
+                        {
+                            notification.userId = parts.userId;
+                            notification.to = parts.group.members;
+                            for (var i = 0; i < notification.to.length; i++) {
+                                notification.seen[i] = { seen: false, id: notification.to[i].toString() };
                             }
-                            notification.groupId=parts.group._id;
-                            notification.message=msgs[2];
+                            notification.groupId = parts.group._id;
+                            notification.message = msgs[2];
                         }
-                            break;
+                        break;
 
-                        case 3 :{
-                            notification.to=friends;
-                            for (var i=0;i<notification.to.length;i++){
-                                notification.seen[i]={seen:false,id:notification.to[i]};
+                    case 3:
+                        {
+                            notification.to = friends;
+                            for (var i = 0; i < notification.to.length; i++) {
+                                notification.seen[i] = { seen: false, id: notification.to[i].toString() };
                             }
-                            notification.userId=parts.user;
-                            notification.groupId=parts.group._id;
-                            notification.message=msgs[3];
+                            notification.userId = parts.user;
+                            notification.groupId = parts.group._id;
+                            notification.message = msgs[3];
                         }
-                            break;
+                        break;
 
-                        case 4 :{
-                            var invited =[]
-                            parts.order.invitedfriends.forEach(function (f) { invited.push(f._id) });
-                            notification.to=invited;
-                            for (var i=0;i<notification.to.length;i++){
-                                notification.seen[i]={seen:false,id:notification.to[i]};
+                    case 4:
+                        {
+                            var invited = []
+                            parts.order.invitedfriends.forEach(function(f) { invited.push(f._id) });
+                            notification.to = invited;
+                            for (var i = 0; i < notification.to.length; i++) {
+                                notification.seen[i] = { seen: false, id: notification.to[i].toString() };
                             }
-                            notification.orderId=parts.order._id;
-                            notification.userId=parts.user;
-                            notification.message=msgs[4];
+                            notification.orderId = parts.order._id;
+                            notification.userId = parts.user;
+                            notification.message = msgs[4];
                         }
-                            break;
+                        break;
 
-                        case 5 :{
-                            notification.to=friends;
-                            for (var i=0;i<notification.to.length;i++){
-                                notification.seen[i]={seen:false,id:notification.to[i]};
+                    case 5:
+                        {
+                            notification.to = friends;
+                            for (var i = 0; i < notification.to.length; i++) {
+                                notification.seen[i] = { seen: false, id: notification.to[i].toString() };
                             }
-                            notification.orderId=parts.order._id;
-                            notification.userId=parts.user;
-                            notification.message=msgs[5];
+                            notification.orderId = parts.order._id;
+                            notification.userId = parts.user;
+                            notification.message = msgs[5];
                         }
-                            break;
+                        break;
 
-                        case 6 :{
-                            notification.to=parts.order.joined;
-                            for (var i=0;i<notification.to.length;i++){
-                                notification.seen[i]={seen:false,id:notification.to[i]};
+                    case 6:
+                        {
+                            notification.to = parts.order.joined;
+                            for (var i = 0; i < notification.to.length; i++) {
+                                notification.seen[i] = { seen: false, id: notification.to[i].toString() };
                             }
-                            notification.orderId=parts.order._id;
-                            notification.message=msgs[6];
+                            notification.orderId = parts.order._id;
+                            notification.message = msgs[6];
 
                         }
-                            break;
+                        break;
 
-                        case 7 :{
-                            notification.to=parts.order.joined;
-                            for (var i=0;i<notification.to.length;i++){
-                                notification.seen[i]={seen:false,id:notification.to[i]};
+                    case 7:
+                        {
+                            notification.to = parts.order.joined;
+                            for (var i = 0; i < notification.to.length; i++) {
+                                notification.seen[i] = { seen: false, id: notification.to[i].toString() };
                             }
-                            notification.orderId=parts.order._id;
-                            notification.message=msgs[7];
+                            notification.orderId = parts.order._id;
+                            notification.message = msgs[7];
                         }
-                            break;
+                        break;
 
-                        case 8 :{
-                            notification.to=parts.userId;
-                            notification.seen[0]={seen:false,id:notification.to[i]}
-                            notification.userId=parts.user;
-                            notification.message=msgs[8];
+                    case 8:
+                        {
+                            notification.to = parts.userId;
+                            notification.seen[0] = { seen: false, id: parts.userId.toString() }
+                            notification.userId = parts.user;
+                            notification.message = msgs[8];
                         }
-                            break;
+                        break;
 
-                        case 9:{
-                            notification.to=[];
+                    case 9:
+                        {
+                            notification.to = [];
 
-                            notification.to.push.apply(notification.to,parts.group.members);
+                            notification.to.push.apply(notification.to, parts.group.members);
 
-                            for (var i=0;i<notification.to.length;i++){
-                                notification.seen[i]={seen:false,id:notification.to[i]};
+                            for (var i = 0; i < notification.to.length; i++) {
+                                notification.seen[i] = { seen: false, id: notification.to[i] };
                             }
-                            notification.orderId=parts.order._id;
-                            notification.groupId=parts.group._id;
+                            notification.orderId = parts.order._id;
+                            notification.groupId = parts.group._id;
                         }
-                            break;
+                        break;
 
-                        default:break;
+                    default:
+                        break;
 
+                }
+                console.log('after', notification);
+
+                var notifModel = mongoose.model("notifications");
+                var notif = new notifModel(notification);
+                notif.save(function(err) {
+                    if (!err) {
+                        console.log("success");
+                    } else {
+                        console.log("Error", err);
                     }
-                    console.log('after',notification);
 
-                    var notifModel=mongoose.model("notifications");
-                    var notif=new notifModel(notification);
-                    notif.save(function (err) {
-                        if(!err){
-                            console.log("success");
-                        }else{
-                            console.log("Error",err);
-                        }
-
-                    });
+                });
 
 
-                })
+            })
 
 
 
 
-            }
-            else console.log('error',err);
-        });
+        } else console.log('error', err);
+    });
 
 
 }
@@ -218,23 +228,21 @@ io.on("connection", function(client) {
 
 
 router.get('/', function(request, response) {
-    //get user id
-    //var id='58e75d0ef48126268d2dc6c6';
 
-    var id = request.token._id;
 
-    mongoose.model('notifications').find({ seen: {$elemMatch: {seen:false, id:request.token._id}}}).populate(' userId groupId orderId usersId', ['username', 'name']).exec(function(err, notif) {
-        var notifff=[];
+    mongoose.model('notifications').find({ to: request.token._id }).populate(' userId groupId orderId usersId', ['username', 'name']).exec(function(err, notif) {
+        var notifff = [];
         notif.forEach(function(notification) {
 
             var notif = notifMsg(notification);
             notifff.push(notif);
             console.log(notif);
         })
-        request.json(notifff);
+        response.json(notifff);
     })
 
 })
+
 
 router.get('/activities', function(request, response) {
 
